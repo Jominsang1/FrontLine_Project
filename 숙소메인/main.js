47358c6c3309a36b2c6077de7628f278ea2208b2
DESKTOP-JDK9D6U\User <pch08040@gmail.com>
2023-09-08 11:09:41 +0900

    숙소메인_업데이트

---
+++ 숙소메인/main.js
@@ -0,0 +1,59 @@
+
+window.addEventListener("load", bind);
+
+    function bind(){
+            // 여행지 우클릭 슬라이드
+            let currentSlide = 1;
+            console.log(23)
+
+
+            document.getElementById('btn2').addEventListener('click', function () {
+                console.log(1)
+                // let counter = 1;
+                // console.log(counter)
+                let slides = document.querySelector('.slide_list')
+                let slideimg = document.querySelectorAll('.slide_item').length;
+                let imgcount = document.querySelector('#count')
+                if (currentSlide == 4) {
+                    imgcount.innerHTML = ("1" + " " + "/" + " " + "4")
+                } else {
+                    imgcount.innerHTML = (currentSlide + 1 + " " + "/" + " " + 4)
+                }
+                // counter =-1;
+                // console.log(slideimg)
+                console.log(currentSlide);
+                if (currentSlide >= slideimg - 1) {
+                    currentSlide = 0;
+                }
+                slides.style.transform = "translateX(-" + currentSlide * "550" + "px)";
+                slides.style.transition = "transform 0.4s ease-in-out";
+                currentSlide++;
+            });
+
+            // 여행지 좌클릭 슬라이드           
+            // let currentSlide = 1;
+
+            document.getElementById('btn1').addEventListener('click', function () {
+                let slides = document.querySelector('.slide_list')
+                let slideimg = document.querySelectorAll('.slide_item').length;
+                let imgcount = document.querySelector('#count')
+                if (currentSlide == 1) {
+                    imgcount.innerHTML = (4 + " " + "/" + " " + 4)
+                } else {
+                    imgcount.innerHTML = (currentSlide - 1 + " " + "/" + " " + 4)
+                }
+                console.log("abc :", imgcount)
+                // console.log('no2 :',slideimg)
+                // console.log('no2 :', currentSlide)
+                if (currentSlide === 1) {
+                    console.log(currentSlide)
+                    currentSlide = slideimg - 2;
+                } else {
+                    currentSlide--;
+                }
+                slides.style.transform = "translateX(-" + (currentSlide - 1) * "550" + "px)";
+                slides.style.transition = "transform 0.4s ease-in-out";
+
+            });
+
+        }
